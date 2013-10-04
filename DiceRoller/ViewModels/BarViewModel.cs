1c54b2b67a1b3580a5e5526145f2642919a9dec4
Alastair Jones <alastair.peter@gmail.com>
2013-10-04 22:14:34 +0100

    Application bar framework

---
+++ DiceRoller/ViewModels/BarViewModel.cs
@@ -0,0 +1,70 @@
+﻿using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+
+namespace DiceRoller.ViewModels
+{
+    public class BarViewModel : ViewModelBase
+    {
+        private int index;
+        private ObservableCollection<BarItemViewModel> buttons;
+        private ObservableCollection<BarItemViewModel> items;
+
+        public BarViewModel()
+        {
+            Update();
+        }
+
+        public int SelectedIndex
+        {
+            get { return index; }
+            set
+            {
+                if (index != value)
+                {
+                    index = value;
+                    RaisePropertyChanged("SelectedIndex");
+                    Update();
+                }
+            }
+        }
+
+        public ObservableCollection<BarItemViewModel> Buttons
+        {
+            get { return buttons ?? (buttons = new ObservableCollection<BarItemViewModel>()); }
+        }
+
+        public ObservableCollection<BarItemViewModel> MenuItems
+        {
+            get { return items ?? (items = new ObservableCollection<BarItemViewModel>()); }
+        }
+
+        private void Update()
+        {
+            while (Buttons.Count > 0) Buttons.RemoveAt(0);
+            while (MenuItems.Count > 0) MenuItems.RemoveAt(0);
+            // NOTE: The Clear() method is not listened to by the Bindable Application Bar
+            //       see: https://bindableapplicationb.codeplex.com/workitem/446
+            //Buttons.Clear();
+            //MenuItems.Clear();
+
+            switch (SelectedIndex)
+            {
+                case 0:
+                    Buttons.Add(new BarItemViewModel { Text = "roll", IconUri = "/Assets/Images/check.png" });
+                    Buttons.Add(new BarItemViewModel { Text = "favorite", IconUri = "/Assets/Images/favs.png" });
+                    Buttons.Add(new BarItemViewModel { Text = "reset", IconUri = "/Assets/Images/delete.png" });
+                    MenuItems.Add(new BarItemViewModel { Text = "settings" });
+                    break;
+
+                case 1:
+                    Buttons.Add(new BarItemViewModel { Text = "select", IconUri = "/Assets/Images/list.png" });
+                    MenuItems.Add(new BarItemViewModel { Text = "clear history" });
+                    MenuItems.Add(new BarItemViewModel { Text = "settings" });
+                    break;
+
+                case 2:
+                    break;
+            }
+        }
+    }
+}
--- DiceRoller/ViewModels/MainViewModel.cs
+++ DiceRoller/ViewModels/MainViewModel.cs
@@ -4,8 +4,14 @@ namespace DiceRoller.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        private BarViewModel bar;
         private HistoryViewModel history;
         private PickViewModel pick;
+
+        public BarViewModel Bar
+        {
+            get { return bar ?? (bar = new BarViewModel()); }
+        }
 
         public HistoryViewModel History
         {
