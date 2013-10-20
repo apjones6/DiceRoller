204eecee4bba3fb77a7f430b47d3b9aa534aa423
Alastair Jones <alastair.peter@gmail.com>
2013-10-20 01:38:58 +0100

    Performance enhancement to info view

---
+++ DiceRoller/Converters/JoinArrayConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DiceRoller.Converters
+{
+    public class JoinArrayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var enumerable = value as IEnumerable;
+            if (enumerable != null)
+            {
+                return string.Join(parameter as string ?? " ", enumerable.OfType<object>().Select(x => x.ToString()));
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
