bfd3d000b93fd3e93a33fd3896b0f6b4fda02957
Alastair Jones <alastair.peter@gmail.com>
2014-02-09 12:41:40 +0000

    State automatically saves on favorites changes
    Also prevents repeated saves when no modifications have occurred. Only
    lightly tested so far.

--- DiceRoller/Models/State.cs
+++ DiceRoller/Models/State.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO.IsolatedStorage;
 using System.Linq;
+using System.Threading;
 
 namespace DiceRoller.Models
 {
@@ -12,7 +17,9 @@ namespace DiceRoller.Models
 
         private static ObservableCollection<Pool> favorites;
         private static bool isLoaded;
+        private static bool isModified;
         private static IsolatedStorageSettings storage;
+        private static Timer timer;
 
         public static ObservableCollection<Pool> Favorites
         {
@@ -37,7 +44,13 @@ namespace DiceRoller.Models
             // Load properties from storage
             favorites = new ObservableCollection<Pool>(storage.Contains(KEY_FAVORITES) ? (Pool[])storage[KEY_FAVORITES] : new Pool[0]);
 
-            // TODO: Attach events so we can auto update storage on changes
+            // Attach events so we can auto update storage on changes
+            favorites.CollectionChanged += ListenToCollectionItems;
+            favorites.CollectionChanged += QueueSave;
+
+            // Attach events to collection items, as they don't bubble
+            ListenToCollectionItems(favorites);
+
             // TODO: Ensure storage is not updated more than once when a number of items are changed in close succession
 
             isLoaded = true;
@@ -45,9 +58,51 @@ namespace DiceRoller.Models
 
         public static void Save()
         {
-            // TODO: Track the changes since last save, and only store necessary properties
-            storage[KEY_FAVORITES] = favorites.ToArray();
-            storage.Save();
+            Debug.WriteLine("State.Save() call - {0}", isModified ? "Has Changes" : "No Changes");
+
+            // TODO: Handle concurrency
+            if (isModified)
+            {
+                // Update in storage
+                storage[KEY_FAVORITES] = favorites.ToArray();
+                storage.Save();
+
+                isModified = false;
+            }
+        }
+
+        private static void ListenToCollectionItems(object sender, NotifyCollectionChangedEventArgs e = null)
+        {
+            if (e != null)
+            {
+                if (e.NewItems != null)
+                {
+                    foreach (INotifyPropertyChanged item in e.NewItems) item.PropertyChanged += QueueSave;
+                }
+
+                if (e.OldItems != null)
+                {
+                    foreach (INotifyPropertyChanged item in e.OldItems) item.PropertyChanged -= QueueSave;
+                }
+            }
+            else
+            {
+                var collection = (ICollection)sender;
+                foreach (INotifyPropertyChanged item in collection) item.PropertyChanged += QueueSave;
+            }
+        }
+
+        private static void QueueSave(object sender, EventArgs e)
+        {
+            isModified = true;
+            if (timer == null)
+            {
+                timer = new Timer(s => Save(), null, 1000, Timeout.Infinite);
+            }
+            else
+            {
+                timer.Change(0, Timeout.Infinite);
+            }
         }
     }
 }
